6160de99fd39dfb179ec08bb571bb8d543af356e
vixxjy <ajorvictor48@gmail.com>
2018-09-15 17:06:45 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SampleComponent } from './sample/sample.component';
+import { NavbarComponent } from './navbar.component';
+import { ImageListComponent } from './sample/image-list/image-list.component';
+import { ImageComponent } from './sample/image-list/image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SampleComponent,
+    NavbarComponent,
+    ImageListComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule
